a107e8aac6ff9d98cb5bb2b077ffd4675cf1d71e
Steffen Winde <inserv@winde-ganzig.de>
2021-12-10 16:22:21 +0100

    edit scss

--- Gruntfile.js
+++ Gruntfile.js
@@ -75,6 +75,8 @@ module.exports = function (grunt) {
 					'<%= project.out %><%= project.theme %>/src/js/script.min.js': [
 						'<%= project.dev %>node_modules/jquery/dist/jquery.min.js',
 						'<%= project.dev %>build/js/jquery/patch.js',
+						'<%= project.dev %>build/vendor/jquery-ui/js/jquery-ui.js',
+						'<%= project.dev %>node_modules/popper.js/dist/umd/popper.min.js',
 						'<%= project.dev %>node_modules/bootstrap/dist/js/bootstrap.bundle.js',
 						'<%= project.dev %>build/vendor/jquery-ui/js/jquery-ui.js',
 						'<%= project.dev %>build/vendor/jquery-unveil/js/jquery.unveil.js',
